7bf881fec9cec121f80c4fb7c8865c22f3b9b733
Michael Rixon <mrixon95@gmail.com>
2021-03-12 00:23:44 +1100

    create and get answers works well

--- src/controllers/Answer_controller.py
+++ src/controllers/Answer_controller.py
@@ -1,9 +1,14 @@
+
 from schemas.AnswerSchema import answer_schema
 from schemas.AnswerSchema import answers_schema
 
 from models.User import User
-from models.Answer import Answer
+from models.SurveyQuestion import SurveyQuestion
 from models.MentalHealthSurvey import MentalHealthSurvey
+from models.Question import Question
+from models.Option import Option
+from models.Answer import Answer
+
 
 from main import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -17,7 +22,7 @@ answer = Blueprint('answer', __name__, url_prefix="/answer")
 def answer_all():
 
     answers = Answer.query.all()
-    return jsonify(answer_schema.dump(answers))
+    return jsonify(answers_schema.dump(answers))
 
 
 
@@ -25,12 +30,33 @@ def answer_all():
 @jwt_required
 def answer_create():
 
-    answer_inputted_fields = answer_schema.load(request.json)
+    email_of_jwt = get_jwt_identity()
+
+    user_of_jwt = User.query.filter_by(email=email_of_jwt).first()
+
+
+    user_id = user_of_jwt.id
+    question_id = request.json["question_id"]
+    option_number = request.json["option_number"]
+
+
+    answer_object = Answer.query.filter_by(user_id=user_id, question_id=question_id).first()
+    
+    if answer_object:
+        return abort(401, description=f"There already exists an answer for this user and for question id {question_id}")
+
+
+    option_object = Option.query.filter_by(question_id=question_id, option_number=option_number).first()
+
+    if not option_object:
+        return abort(401, description=f"There does not exists a response with question id {question_id} and option number {option_number}")
+
+
 
     answer_from_fields = Answer()
-    answer_from_fields.user_id = answer_inputted_fields["user_id"]
-    answer_from_fields.question_id = answer_inputted_fields["question_id"]
-    answer_from_fields.option_id = answer_inputted_fields["option_id"]
+    answer_from_fields.user_id = user_id
+    answer_from_fields.question_id = question_id
+    answer_from_fields.option_number = option_number
 
     db.session.add(answer_from_fields)
     
@@ -50,40 +76,39 @@ def answer_create():
 #     return jsonify(mental_health_survey_schema.dump(mentalhealthsurvey_object))
 
 
-@answer.route("/<int:user_id>/<int:question_id>", methods=["PUT", "PATCH"])
-@jwt_required
-def answer_update(user_id, question_id):
+@answer.route("/", methods=["PUT", "PATCH"])
+def answer_update():
 
-    jwt_username = get_jwt_identity()
-    jwt_user = User.query.get(jwt_username)
 
-    answer_fields = answer_schema.load(request.json, partial=True)
+    question_id = request.json["question_id"]
+    option_id = request.json["option_id"]
+    option_text = request.json["option_text"]
 
-    if not jwt_user:
-        return abort(401, description="Invalid user")
+    answer_object = Answer.query.filter_by(question_id=question_id, option_number=option_id).first()
+    
+    if answer_object is None:
+        return abort(401, description=f"There does not exist an option with id {option_id} and question id {question_id}")
+    
 
-    answer_object = Answer.query.filter_by(user_id=user_id, question_id=question_id).first()
+    answer_object.option_text = option_text
 
-    if not answer_object:
-        return abort(401, description=f"Answer answered by user with id {user_id} and with question id {question_id} does not exist")
-
-    answer_object.update(answer_fields)
     db.session.commit()
 
     return jsonify(answer_schema.dump(answer_object))
 
 
 
-@answer.route("/<int:user_id>/<int:question_id>", methods=["DELETE"])
-@jwt_required
-def answer_delete(user_id, question_id):
+@answer.route("/", methods=["DELETE"])
+def answer_delete():
 
-    jwt_username = get_jwt_identity()
+    question_id = request.json["question_id"]
+    option_id = request.json["option_id"]
 
-    answer_object = Option.query.filter_by(user_id=user_id, question_id=question_id).first()
-
-    if not answer_object:
-        return abort(401, description=f"Answer answered by user with id {user_id} and with question id {question_id} does not exist")
+    answer_object = Answer.query.filter_by(question_id=question_id, option_number=option_id).first()
+    
+    if answer_object is None:
+        return abort(401, description=f"There does not exist an answer with id {option_id} and question id {question_id}")
+    
 
     json_object_to_return = jsonify(answer_schema.dump(answer_object))
 
